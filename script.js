c05dac0da4a9ead3b35a31d1358a444444dc3ac2
Najoule Morrris <najoulemorrris@Najoules-Mac-mini.local>
2026-07-07 10:04:26 -0400

    Deleted images folder

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const emailBtn = document.getElementById("email");
 const gitHubBtn = document.getElementById("gitHub");
 
 const emailBtn = document.addEventListener
+
+const gitHubBtnBtn = document.addEventListener("mail-to: mnajoule@hotmail.com")
